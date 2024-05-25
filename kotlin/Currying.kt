6761c74abae9090948a3892bffb73e750c001236
Torsten Schlieder <torsten.schlieder@unite.eu>
2024-05-25 19:22:49 +0200

    Kotlin

---
+++ kotlin/Currying.kt
@@ -0,0 +1,14 @@
+fun log(level: String, message: String) {
+    println("[$level]: $message")
+}
+
+fun logForLevel(level: String): (String) -> Unit {
+    return fun (message: String) {
+        log(level, message)
+    }
+}
+
+fun main() {
+    val logDebug = logForLevel("DEBUG")
+    logDebug("Hello, World!")  // [DEBUG] Hello, World!
+}
---
+++ kotlin/Lambda.kt
@@ -0,0 +1,4 @@
+fun main() {
+    val list = listOf(1, 3, 5)
+    list.forEach { i -> println("Item $i") }
+}
